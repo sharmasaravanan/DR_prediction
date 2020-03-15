bb54e2d2d0143925c92997c8c56888f0f254a3c8
sharma <sharmasaravanan.1993@gmail.com>
2020-03-15 19:49:22 +0530

    updated file

--- prediction.py
+++ prediction.py
@@ -1,62 +1,70 @@
 import numpy as np
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import cv2
-import keras
 import tensorflow as tf
 from tensorflow.keras.models import model_from_json
 import imutils
 
-# loading model
-model_architecture = 'CNN_Model.json'
-model_weights = 'model.h5'
-global graph, model
-graph = tf.get_default_graph()
-model = model_from_json(open(model_architecture).read())
-model.load_weights(model_weights)
 
-# model.summary() # uncomment if you want to see the model structure
+class DR:
+    def __init__(self):
+        # loading model
+        model_architecture = 'CNN_Model.json'
+        model_weights = 'model.h5'
+        self.graph = tf.get_default_graph()
+        self.model = model_from_json(open(model_architecture).read())
+        self.model.load_weights(model_weights)
 
-# setting image params
-height = 224
-width = 224
-channels = 3
+        # self.model.summary() # uncomment if you want to see the model structure
 
-def prediction(img):
-    image = cv2.imread(img)
-    image = cv2.resize(image, (height, width))
-    data = np.expand_dims(image, axis=0)
-    with graph.as_default():
-        results = model.predict(data)
-    print(results)
-    print('D-Retinopathy : {}%'.format(round(results[1]*100,2)))
-    print('Normal : {}% '.format(round(results[0]*100,2)))
+        # setting image params
+        self.height = 224
+        self.width = 224
+        self.channels = 3
+        self.level = None
 
-    proba = round(results[1]*100,2)
-    output = imutils.resize(image, width=400)
-    if 50 < proba <= 65:
-        level = "Mild"
-    elif 65 < proba <= 80:
-        level = "Moderate"
-    elif 80 < proba <= 90:
-        level = "Severe"
-    elif proba > 90:
-        level = "Proliferative"
-    else:
-        level = None
+    def prediction(self,img):
+        self.img = img
+        self.image = cv2.imread(self.img)
+        self.image = cv2.resize(self.image, (self.height, self.width))
+        self.data = np.expand_dims(self.image, axis=0)
 
-    if proba > 50:
-        out = "{}: {:.2f}%".format('D-Retinopathy', proba)
-        color = (0,255,0)
-    else:
-        out = "{}".format("Normal")
-        color = (255,0,0)
-    text = cv2.putText(output, out, (10, 25),  cv2.FONT_HERSHEY_SIMPLEX, 0.7, color, 2)
-    cv2.putText(output, "Level: {}".format(level), (10, 65),  cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 2)
-    plt.figure(num='Result')
-    plt.title("Predicted results")
-    plt.axis('off')
-    plt.imshow(output)
-    plt.show()
-    return out, level
+        with self.graph.as_default():
+            self.results = self.model.predict(self.data)[0]
 
-# prediction("/home/sharma/Downloads/mild.jpg")
+        print('D-Retinopathy : {}%'.format(round(self.results[1]*100,2)))
+        print('Normal : {}% '.format(round(self.results[0]*100,2)))
+
+        self.proba = round(self.results[1]*100,2)
+        if 50 < self.proba <= 65:
+            self.level = "Mild"
+        elif 65 < self.proba <= 80:
+            self.level = "Moderate"
+        elif 80 < self.proba <= 90:
+            self.level = "Severe"
+        elif self.proba > 90:
+            self.level = "Proliferative"
+
+        if self.proba > 50:
+            self.out = "{}: {:.2f}%".format('D-Retinopathy', self.proba)
+            self.color = (0,255,0)
+        else:
+            self.out = "{}".format("Normal")
+            self.color = (255,0,0)
+        self._display()
+        return self.out, self.level
+
+    def _display(self):
+        output = imutils.resize(self.image, width=400)
+        cv2.putText(output, self.out, (10, 25),  cv2.FONT_HERSHEY_SIMPLEX, 0.7, self.color, 2)
+        cv2.putText(output, "Level: {}".format(self.level), (10, 65),  cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 2)
+        cv2.imwrite("./templates/output.jpeg", output)
+        # plt.figure(num='Result')
+        # plt.title("Predicted results")
+        # plt.axis('off')
+        # plt.imshow(output)
+        # plt.show()
+
+
+
+
--- webapp.py
+++ webapp.py
@@ -1,21 +1,45 @@
 from flask import Flask, request, render_template
 from werkzeug.utils import secure_filename
-from prediction import prediction
-
+from prediction import DR
+import cv2
 app = Flask(__name__)
 
 
 @app.route("/", methods=["POST", "GET"])
 def predict():
+    dr = DR()
     if request.method == 'GET':
         return render_template('index.html')
     elif request.method == "POST":
-        image = request.files.get('imgup')
-        image.save('./' + secure_filename(image.filename))
-        name, score = prediction(image.filename)
-        # name,score = prediction("/home/sharma/Downloads/mild.jpg")
-        kwargs = {'name': name, 'score': score}
-        return render_template('index2.html', **kwargs)
+        if "button" in request.form:
+            if request.form['button'] == "Capture":
+                cam = cv2.VideoCapture(0)
+                while True:
+                    ret, frame = cam.read()
+                    org = frame.copy()
+                    cv2.putText(frame, "Press 'Esc' to close or 'c' to capture.", (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 1,
+                                (0, 0, 255), 2)
+                    cv2.imshow("Image Capturing System", frame)
+                    if not ret:
+                        break
+                    k = cv2.waitKey(1)
+                    if k % 256 == 27:
+                        break
+                    elif k % 256 == 99:
+                        cv2.imwrite("testImage.jpg", org)
+                        break
+                cam.release()
+                cv2.destroyAllWindows()
+                name, score = dr.prediction("testImage.jpg")
+                kwargs = {'name': name, 'score': score}
+                return render_template('index2.html', **kwargs)
+        else:
+            image = request.files.get('imgup')
+            image.save('./' + secure_filename(image.filename))
+            img = image.filename
+            name, score = dr.prediction(img)
+            kwargs = {'name': name, 'score': score}
+            return render_template('index2.html', **kwargs)
 
 
 if __name__ == '__main__':
